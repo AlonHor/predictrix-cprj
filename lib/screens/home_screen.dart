72a6f81e73ddbaae3fe5ab1bcb6b629f09ac50ec
Alon Horesh <57628667+AlonHor@users.noreply.github.com>
2025-05-23 10:03:30 +0300

    fix: add "Guest" title instead of null

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,62 +9,65 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black87,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Hello, ${FirebaseAuth.instance.currentUser?.displayName}",
-                style: const TextStyle(fontSize: 24),
-              ),
+      backgroundColor: Colors.black87,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Hello, ${FirebaseAuth.instance.currentUser?.displayName ?? "Guest"}",
+              style: const TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: SizedBox(
+          width: 72,
+          height: 72,
+          child: FittedBox(
+              child: FloatingActionButton.large(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              // Navigator.pushReplacementNamed(context, "/");
+            },
+            child: const Icon(Icons.exit_to_app, size: 42),
+          ))),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: SizedBox(
+        height: 72,
+        child: Container(
+          decoration: const BoxDecoration(
+              border: Border(top: BorderSide(color: Colors.grey, width: 0.5))),
+          child: BottomNavigationBar(
+            elevation: 8,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    size: 32,
+                  ),
+                  label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Hero(
+                      tag: "chats-icon",
+                      child: Material(
+                          color: Colors.transparent,
+                          child: Icon(Icons.chat, size: 32))),
+                  label: "Chats"),
             ],
+            onTap: (index) {
+              switch (index) {
+                case 0:
+                  // Navigator.pushNamed(context, "/");
+                  break;
+                case 1:
+                  NavigatorUtils.navigateTo(context, const ChatsPage());
+                  break;
+              }
+            },
           ),
         ),
-        floatingActionButton: SizedBox(
-            width: 72,
-            height: 72,
-            child: FittedBox(
-                child: FloatingActionButton.large(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-                // Navigator.pushReplacementNamed(context, "/");
-              },
-              child: const Icon(Icons.exit_to_app, size: 42),
-            ))),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: SizedBox(
-            height: 72,
-            child: Container(
-                decoration: const BoxDecoration(
-                    border: Border(
-                        top: BorderSide(color: Colors.grey, width: 0.5))),
-                child: BottomNavigationBar(
-                    elevation: 8,
-                    items: const [
-                      BottomNavigationBarItem(
-                          icon: Icon(
-                            Icons.home,
-                            size: 32,
-                          ),
-                          label: "Home"),
-                      BottomNavigationBarItem(
-                          icon: Hero(
-                              tag: "chats-icon",
-                              child: Material(
-                                  color: Colors.transparent,
-                                  child: Icon(Icons.chat, size: 32))),
-                          label: "Chats"),
-                    ],
-                    onTap: (index) {
-                      switch (index) {
-                        case 0:
-                          // Navigator.pushNamed(context, "/");
-                          break;
-                        case 1:
-                          NavigatorUtils.navigateTo(context, const ChatsPage());
-                          break;
-                      }
-                    }))));
+      ),
+    );
   }
 }
